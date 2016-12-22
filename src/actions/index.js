de718a6c0c5500ebe6ed45b204f3e90d1e4738ee
Yitaek Hwang <yitaek@gmail.com>
2016-12-22 17:36:25 -0500

    connect the UI to the backend

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,6 +1,14 @@
 import axios from 'axios';
+import { browserHistory } from 'react-router';
+import { AUTH_USER,
+	AUTH_ERROR,
+	UNAUTH_USER,
+	FETCH_MESSAGE
+} from './types';
 
 const ROOT_URL = 'http://localhost:3090';
+
+// using redux thunk to get back a function 
 
 export function signinUser({ email, password }){
 	// redux thunk allows you to return a function instead of object
@@ -8,14 +16,68 @@ export function signinUser({ email, password }){
 	return function(dispatch) {
 		// Submit email/password to the server
 		axios.post(`${ROOT_URL}/signin`, { email, password })
+			.then(response => {
+				// If request is good...
+				// - update state to indicate user is authenticated
+				dispatch({ type: AUTH_USER });
+				// - save the JWT token 
+				localStorage.setItem('token', response.data.token);
+				// - redirect ot the route '/feature'
+				browserHistory.push('/feature');
 
-		// If request is good...
-		// - update state to indicate user is authenticated
-		// - save the JWT token 
-		// - redirect ot the route '/feature'
+			})
+			.catch(() => {
+				// If request is bad...
+				// - show an error to the user 
+				dispatch(authError('Bad Login Info'));
+			});
 
-		// If request is bad...
-		// - show an error to the user 
+		// axios always returns a promise
 	}
+}
+
+export function signupUser({ email, password }){
+	return function(dispatch){
+		axios.post(`${ROOT_URL}/signup`, { email, password })
+			.then(response => {
+				dispatch({ type: AUTH_USER });
+				console.log(response)
+				localStorage.setItem('token', response.data.token);
+				browserHistory.push('/feature');
+			})
+			.catch((err) => {
+				//console.log(JSON.stringify(err.response))
+				dispatch(authError(err.response.data.error))
+			});
+	}
+}
+
+export function signoutUser(){
+	// delete the JWT token
+	localStorage.removeItem('token');
 	
+	return {
+		type: UNAUTH_USER
+	};
+}
+
+export function authError(error){
+	return {
+		type: AUTH_ERROR, 
+		payload: error
+	}
+}
+
+export function fetchMessage(){
+	return function(dispatch){
+		axios.get(ROOT_URL, {
+			headers: { authorization: localStorage.getItem('token')}
+		})
+		.then(response => {
+			dispatch({
+				type: FETCH_MESSAGE,
+				payload: response.data.message
+			});
+		})
+	}
 }
--- src/components/auth/signin.js
+++ src/components/auth/signin.js
@@ -7,6 +7,16 @@ class Signin extends Component{
 		this.props.signinUser({ email, password});
 	}
 
+	renderAlert(){
+		if (this.props.errorMessage){
+			return (
+				<div className="alert alert-danger">
+					<strong>Oops!</strong> {this.props.errorMessage}
+				</div>
+			)
+		}
+	}
+
 	render() {
 		const { handleSubmit, fields: {email, password }} = this.props;
 
@@ -16,15 +26,19 @@ class Signin extends Component{
 					<label>Email:</label>
 					<input {...email} className="form-control" /> 
 					<label>password:</label>
-					<input {...password} className="form-control" /> 
+					<input {...password} type="password" className="form-control" /> 
 				</fieldset> 
+				{this.renderAlert()}
 				<button action="submit" className="btn btn-primary">Sign in</button>
 			</form>
 		);
 	}
 }
+function mapStateToProps (state) {
+	return { errorMessage: state.auth.error };
+}
 
 export default reduxForm({
 	form: 'signin',
 	fields: ['email', 'password']
-}, null, actions)(Signin);
+}, mapStateToProps, actions)(Signin);
